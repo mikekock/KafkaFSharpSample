a091a368e606f6860abffb9d1ba012036224f9f1
Mike Kock <mikekock@gmail.com>
2016-08-26 06:15:10 -0700

    Initial commit

---
+++ KafkaConsumer/Program.fs
@@ -0,0 +1,94 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+open System
+open System.Text
+open System.Collections.Generic
+open RdKafka
+
+type Input = 
+    | Val of string
+
+//type Battle with
+//    static member fromJSON json =
+//        let jo = JObject.Parse(json)
+//        let result:Battle = {
+//            Dtstamp = Guid(jo.["Dtstamp"].ToString()); 
+//            Id = Guid(jo.["Id"].ToString()); 
+//            Attacker = Guid(jo.["Attacker"].ToString()); 
+//            Defender = Guid(jo.["Defender"].ToString()); 
+//            Winner = Guid(jo.["Winner"].ToString()); 
+//            Loser = Guid(jo.["Loser"].ToString());
+//            }
+//        result
+
+type Output = 
+    | Result of string
+
+let handle input =
+    //let r = sprintf "Output %s" input.Val
+    //Some(Output.Result(r))
+    match input with
+        | Val v ->  Some(Output.Result (sprintf "Output %s" v))
+            
+let interpret output =
+    match output with
+    | Result r -> 
+        printfn "%s" r
+        Some(true)
+                        
+let decode msg =
+    //let eventType = parseEventTypePayload msg
+    Some(Input.Val(msg))
+    //match eventType.EventType with
+    //| "Battle" ->
+    //    Some(Input.Bat(Battle.fromJSON(eventType.Payload)))
+    //| _ -> 
+    //    printfn "Encountered unknown EventType %s" eventType.EventType
+    //    None
+
+
+
+let kafkaConsumer group server topic pipeline = 
+    let config = new Config()
+    config.GroupId <- group
+
+    let consumer:EventConsumer = new EventConsumer(config, server)
+    let decodeKafkaMessageToString (msg:Message) = 
+        Encoding.UTF8.GetString(msg.Payload, 0, msg.Payload.Length)
+    let pipe msg = decodeKafkaMessageToString msg |> pipeline |> ignore
+    
+    consumer.OnMessage.Add(fun msg -> pipe |> ignore)  
+    let topics = new List<string>()
+    topics.Add topic
+    consumer.Subscribe topics
+    consumer.Start()
+    consumer
+
+let (>>=) m f = Option.bind f m
+
+let standardSomeOrNonePipeline decodeInput handleInput interpretOutput message = 
+    message 
+        |> decodeInput
+        >>= handleInput
+        >>= interpretOutput
+
+
+[<EntryPoint>]
+let main argv = 
+    //printfn "%A" argv
+    let consumerGroup = argv.[0]
+
+
+   
+
+
+    let pipeline message = standardSomeOrNonePipeline decode handle interpret message
+
+    let consumer = kafkaConsumer consumerGroup "www.smokinserver.com:9092" "test" pipeline
+
+    printfn "Started consumer, press enter to stop consuming" 
+
+
+    let unused = Console.ReadLine()
+    0 // return an integer exit code
+
---
+++ KafkaProducer/Program.fs
@@ -0,0 +1,62 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+open System
+open System.Text
+open RdKafka
+
+let stringToUTF8Bytes (s:string) =
+    let data = Encoding.UTF8.GetBytes(s)
+    data
+
+let sendToTopic (topic:Topic) (message:byte[]) =
+    message |> topic.Produce
+
+let sendToTopicWithKey (topic:Topic) (message:byte[], key:byte[]) =
+     topic.Produce (message, key)
+
+let createKafkaTopic serverName bufferingMS topicName : Topic = 
+    let c = new Config()
+    c.["queue.buffering.max.ms"] <- bufferingMS;
+    let tc = new TopicConfig()
+    let producer = new Producer(serverName)
+    let topic = producer.Topic(topicName)
+    topic
+
+let kafkaTopic topicName : Topic = 
+    createKafkaTopic "www.smokinserver.com:9092" "10" topicName
+
+[<EntryPoint>]
+let main argv = 
+    let iterations = System.Int32.Parse argv.[0]
+    let topic = kafkaTopic("test")
+    let sendToTestTopic = sendToTopic topic
+    let rnd = System.Random()
+
+    for i in 1 .. iterations do
+
+        let randomNumber = rnd.Next 9
+        let msg = stringToUTF8Bytes (sprintf "%d" i)
+        let key = stringToUTF8Bytes (sprintf "%d" i)
+ 
+        sendToTestTopic msg |> ignore
+        printfn "%d = %d" i randomNumber
+
+    
+    //let c = new Config()
+    //                                            c.["queue.buffering.max.ms"] <- "1";
+    //                                            let tc = new TopicConfig()
+    //                                            let producer = new Producer("www.smokinserver.com:9092")
+    //                                            let topic = producer.Topic("battlers")
+    //                                            
+    //                                            (*let f = JsonConvert.SerializeObject({EventType = "XYZ"; Payload = battle})
+    //                                            Encoding.UTF8.GetBytes(f)
+    //                                            |> topic.Produce |> ignore*)
+    //
+    //                                            let envelope:EventEnvelope<Battle> = {EventType = "Battle"; Payload = battle}
+    //                                            let e = JsonConvert.SerializeObject(envelope)
+    //                                            printfn "Sending to Kafka -> %s" e
+    //                                            Encoding.UTF8.GetBytes(e)
+    //                                            |> topic.Produce |> ignore
+    let unused = Console.ReadLine()
+    0 // return an integer exit code
+
