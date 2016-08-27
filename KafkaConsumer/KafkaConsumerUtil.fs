84b3d66d5ff3395eef761ae917140cf6e43050b3
Mike Kock <mikekock@gmail.com>
2016-08-27 07:44:54 -0700

    Moved common Kafka consumer code into own module

---
+++ KafkaConsumer/KafkaConsumerUtil.fs
@@ -0,0 +1,29 @@
+﻿module KafkaConsumerUtil
+open System.Text
+open System.Collections.Generic
+open RdKafka
+
+let kafkaConsumer group server topic pipeline = 
+    let config = new Config()
+    config.GroupId <- group
+
+    let consumer:EventConsumer = new EventConsumer(config, server)
+    let decodeKafkaMessageToString (msg:Message) = 
+        Encoding.UTF8.GetString(msg.Payload, 0, msg.Payload.Length)
+    let pipe msg = decodeKafkaMessageToString msg |> pipeline |> ignore
+    
+    consumer.OnMessage.Add(fun msg -> msg |> pipe |> ignore) 
+    let topics = new List<string>()
+    topics.Add topic
+    consumer.Subscribe topics
+    consumer.Start()
+    consumer
+
+let (>>=) m f = Option.bind f m
+
+let standardSomeOrNonePipeline decodeInput handleInput interpretOutput message = 
+    message 
+        |> decodeInput
+        >>= handleInput
+        >>= interpretOutput
+
--- KafkaConsumer/Program.fs
+++ KafkaConsumer/Program.fs
@@ -1,6 +1,8 @@
-﻿open System
+﻿module MainKafkaConsumer
+open System
 open System.Text
 open System.Collections.Generic
+open KafkaConsumerUtil
 open RdKafka
 
 type Input = 
@@ -44,29 +46,6 @@ let decode msg =
 
 
 
-let kafkaConsumer group server topic pipeline = 
-    let config = new Config()
-    config.GroupId <- group
-
-    let consumer:EventConsumer = new EventConsumer(config, server)
-    let decodeKafkaMessageToString (msg:Message) = 
-        Encoding.UTF8.GetString(msg.Payload, 0, msg.Payload.Length)
-    let pipe msg = decodeKafkaMessageToString msg |> pipeline |> ignore
-    
-    consumer.OnMessage.Add(fun msg -> msg |> pipe |> ignore) 
-    let topics = new List<string>()
-    topics.Add topic
-    consumer.Subscribe topics
-    consumer.Start()
-    consumer
-
-let (>>=) m f = Option.bind f m
-
-let standardSomeOrNonePipeline decodeInput handleInput interpretOutput message = 
-    message 
-        |> decodeInput
-        >>= handleInput
-        >>= interpretOutput
 
 
 [<EntryPoint>]
